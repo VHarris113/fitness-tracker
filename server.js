6348b2ec960d7f3b1e3286dbdd98fb82910fb551
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-05 19:05:40 -0400

    working on routes

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// const User = require("./userModel.js");
 const app = express();
 
 app.use(logger("dev"));
@@ -17,28 +16,13 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker-project",
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false
   }
 );
 
-app.post("/submit", ({ body }, res) => {
-  const user = new User(body);
-  user.workout();
-  user.continueWorkout();
-
-  newWorkout.create(user)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+app.use(require("./routes/workout"));
+app.use(require("./routes/index"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
-
-console.log("Hello")
