085389ecfed14a4c3da577b241e48d2c5a17f5b3
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-05 20:33:36 -0400

    stuck on figuring out why Cannot GET /exercise appears

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,43 @@
+const router = require("express").Router();
 const db = require("../models");
 
-module.exports = function(app) {
-    app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}).then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
-    });
 
-    app.get("/api/workouts/range", ({}, res) => {
+router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+   });
+});
+
+router.get("/api/workouts/range", ({}, res) => {
         db.Workout.find({}).then((dbWorkout) => {
             res.json(dbWorkout);
         })
         .catch(err => {
             res.status(400).json(err);
         });
-    });
+});
 
-    app.post("/api/workouts/", (req, res) => {
-        db.Workout.create(req.body).then((dbWorkout) => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body).then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
     });
+});
 
-    app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(
-            { _id: req.params.id }, { exercises: req.body }
-        ).then((dbWorkout) => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        { _id: req.params.id }, { exercises: req.body }
+    ).then((dbWorkout) => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
     });
-};
+});
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+router.get("/exercise", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+
+router.get("/stats", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+
+module.exports = router;
