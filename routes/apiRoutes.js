ac253020ccac4dc8d90c7ba36c81218bb1a37091
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-06 11:20:41 -0400

    edited readme and trying to figure out how to fix the duration since the time won't log

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -20,7 +20,7 @@ router.get("/api/workouts/range", ({}, res) => {
         });
 });
 
-router.post("/api/workouts", (req, res) => {
+router.post("/api/workouts/", (req, res) => {
     db.Workout.create(req.body).then((dbWorkout) => {
         res.json(dbWorkout);
     })
