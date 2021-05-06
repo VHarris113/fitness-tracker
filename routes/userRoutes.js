9b1a535f27c1f76c5207efd15a365f51341754c1
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-06 09:58:12 -0400

    fixed the issue with my routing and the cannot get/ error code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const path = require("path");
+const router = require("express").Router();
 
 router.get("/exercise", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
 router.get("/stats", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/stats.html"));
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 
